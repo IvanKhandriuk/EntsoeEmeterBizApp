5a40051060b0ed2b8794fc66bdad6d1092c35d48
Khandriuk Ivan <handryukivan@gmail.com>
2023-01-11 17:00:56 +0200

    add VPAdapter.kt

---
+++ app/src/main/java/com/ikhandriuk/entsoeemeterbizapp/VPAdapter.kt
@@ -0,0 +1,15 @@
+package com.ikhandriuk.entsoeemeterbizapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class VPAdapter(fm: FragmentManager, behavior: Int) : FragmentPagerAdapter(fm, behavior) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
